4a40d60065c2e1657e76c767ba0227612659ade6
Katherine-Gutierrez-2005 <katherinegutierrezgarcia76@gmail.com>
2026-06-12 14:06:32 -0500

    Version 2.0
    Conectar Frontend con url real de Backend

--- App/Frontend/src/services/api.js
+++ App/Frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'backend-projectt1.vercel.app',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+    baseURL: 'https://vercel.app', 
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
 });
-
+  
 export default api; 
