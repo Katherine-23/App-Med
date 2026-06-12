eadb3da1d10c4b8ebd13fd3d77cc9898ea4f315b
Katherine-Gutierrez-2005 <katherinegutierrezgarcia76@gmail.com>
2026-06-12 12:56:16 -0500

    Version 1.9

--- App/Frontend/src/services/api.js
+++ App/Frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: 'backend-projectt1.vercel.app',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
     }
 });
 
-export default api;
+export default api; 
