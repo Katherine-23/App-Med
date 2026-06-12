006c9a903a777bed476aadd2c51b77089516f1f3
Katherine-Gutierrez-2005 <katherinegutierrezgarcia76@gmail.com>
2026-06-12 14:14:37 -0500

    version 2.1
    nuevaa

--- App/Frontend/src/services/api.js
+++ App/Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://vercel.app', 
+    baseURL: 'backend-projectt1.vercel.app', 
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
