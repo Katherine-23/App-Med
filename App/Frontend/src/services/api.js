cdc94d076caa0a094f58f1f420c4dfff0edd181c
katherine23 <jessiejinpachi2005@gmail.com>
2026-06-14 22:04:51 -0500

    Instalar Chart.js para gráficas

--- App/Frontend/src/services/api.js
+++ App/Frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'backend-projectt1.vercel.app', 
+  baseURL: 'https://movilbackend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
   }
 });
-  
-export default api; 
+
+export default api;
